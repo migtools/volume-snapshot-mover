d64caf45dd1e840b8c77da99bd31ed139bf91b80
Savitha Raghunathan <sraghuna@redhat.com>
2022-03-21 14:24:10 -0400

    Adding static pod creation and pvc binding

--- controllers/pvc.go
+++ controllers/pvc.go
@@ -26,6 +26,7 @@ func (r *DataMoverBackupReconciler) BindPVC(log logr.Logger) (bool, error) {
 		return false, errors.New("cloned volumesnapshot not available in the protected namespace")
 	}
 
+	// Create a PVC with the above volumesnapshot as the source
 	pvc := &corev1.PersistentVolumeClaim{
 		ObjectMeta: metav1.ObjectMeta{
 			Name:      fmt.Sprintf("%s-pvc", dmb.Spec.VolumeSnapshotContent.Name),
@@ -33,25 +34,67 @@ func (r *DataMoverBackupReconciler) BindPVC(log logr.Logger) (bool, error) {
 		},
 	}
 
-	op, err := controllerutil.CreateOrUpdate(r.Context, r.Client, pvc, func() error {
+	// check if pvc exists
+	// create pvc only if it doesnt exists
+	_ = r.Get(r.Context, r.NamespacedName, pvc)
 
-		err := controllerutil.SetOwnerReference(&dmb, pvc, r.Scheme)
+	if pvc.Status.Phase == "" {
+
+		op, err := controllerutil.CreateOrUpdate(r.Context, r.Client, pvc, func() error {
+
+			err := controllerutil.SetOwnerReference(&dmb, pvc, r.Scheme)
+			if err != nil {
+				return err
+			}
+			return r.buildPVC(pvc, &vs)
+		})
 		if err != nil {
-			return err
+			r.Log.Info(fmt.Sprintf("error: %v", err))
+			return false, err
 		}
-		return r.buildPVC(pvc, &vs)
-	})
-	if err != nil {
-		return false, err
-	}
-	if op == controllerutil.OperationResultCreated || op == controllerutil.OperationResultUpdated {
+		if op == controllerutil.OperationResultCreated || op == controllerutil.OperationResultUpdated {
 
-		r.EventRecorder.Event(pvc,
-			corev1.EventTypeNormal,
-			"PVCReconciled",
-			fmt.Sprintf("performed %s on PVC %s", op, pvc.Name),
-		)
+			r.EventRecorder.Event(pvc,
+				corev1.EventTypeNormal,
+				"PVCReconciled",
+				fmt.Sprintf("performed %s on PVC %s", op, pvc.Name),
+			)
+		}
+
+		// Bind the above PVC to a dummy pod
+		dp := &corev1.Pod{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      fmt.Sprintf("%s-pod", dmb.Name),
+				Namespace: r.NamespacedName.Namespace,
+			},
+		}
+
+		r.Log.Info(fmt.Sprintf("pod: %v", dp))
+
+		op, err = controllerutil.CreateOrUpdate(r.Context, r.Client, dp, func() error {
+
+			err := controllerutil.SetOwnerReference(&dmb, dp, r.Scheme)
+			if err != nil {
+				r.Log.Info(fmt.Sprintf("error: %v", err))
+				return err
+			}
+			return r.buildPod(pvc, dp)
+		})
+
+		if err != nil {
+			r.Log.Info(fmt.Sprintf("error: %v", err))
+			return false, err
+		}
+		if op == controllerutil.OperationResultCreated || op == controllerutil.OperationResultUpdated {
+
+			r.EventRecorder.Event(dp,
+				corev1.EventTypeNormal,
+				"PodReconciled",
+				fmt.Sprintf("performed %s on pod %s", op, dp.Name),
+			)
+		}
 	}
+
 	return true, nil
 }
 
@@ -72,5 +115,41 @@ func (r *DataMoverBackupReconciler) buildPVC(pvc *corev1.PersistentVolumeClaim, 
 		},
 	}
 	pvc.Spec = pvcspec
+	return nil
+}
+
+func (r *DataMoverBackupReconciler) buildPod(pvc *corev1.PersistentVolumeClaim, p *corev1.Pod) error {
+
+	podspec := corev1.PodSpec{
+		Containers: []corev1.Container{
+			{
+				Name:  "busybox",
+				Image: "quay.io/ocpmigrate/mssql-sample-app:microsoft",
+				Command: []string{
+					"/bin/sh", "-c", "tail -f /dev/null",
+				},
+
+				VolumeMounts: []corev1.VolumeMount{
+					{
+						Name:      "vol1",
+						MountPath: "/mnt/volume1",
+					},
+				},
+			},
+		},
+		Volumes: []corev1.Volume{
+			{
+				Name: "vol1",
+				VolumeSource: corev1.VolumeSource{
+					PersistentVolumeClaim: &corev1.PersistentVolumeClaimVolumeSource{
+						ClaimName: pvc.Name,
+					},
+				},
+			},
+		},
+		RestartPolicy: corev1.RestartPolicyNever,
+	}
+
+	p.Spec = podspec
 	return nil
 }
