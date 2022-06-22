742f778428020fa83ebe0b13d70851f8480091ef
Savitha Raghunathan <sraghuna@redhat.com>
2022-06-22 15:29:44 -0400

    refractor restic secret gen

--- controllers/restic.go
+++ controllers/restic.go
@@ -3,12 +3,11 @@ package controllers
 import (
 	"context"
 	"fmt"
-	"strings"
 
 	"github.com/go-logr/logr"
 	datamoverv1alpha1 "github.com/konveyor/volume-snapshot-mover/api/v1alpha1"
+	resticcommon "github.com/konveyor/volume-snapshot-mover/pkg"
 	corev1 "k8s.io/api/core/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 )
@@ -48,7 +47,7 @@ const (
 	resticSecretName = "restic-secret"
 )
 
-func (r *VolumeSnapshotBackupReconciler) CreateResticSecret(log logr.Logger) (bool, error) {
+func (r *VolumeSnapshotBackupReconciler) CreateVSBResticSecret(log logr.Logger) (bool, error) {
 	// get volumesnapshotbackup from cluster
 	vsb := datamoverv1alpha1.VolumeSnapshotBackup{}
 	if err := r.Get(r.Context, r.req.NamespacedName, &vsb); err != nil {
@@ -63,30 +62,29 @@ func (r *VolumeSnapshotBackupReconciler) CreateResticSecret(log logr.Logger) (bo
 		r.Log.Error(err, "unable to fetch PVC")
 		return false, err
 	}
+	// get restic secret from user
+	resticSecret := corev1.Secret{}
+	if err := r.Get(r.Context, types.NamespacedName{Namespace: r.NamespacedName.Namespace, Name: resticSecretName}, &resticSecret); err != nil {
+		r.Log.Error(err, "unable to fetch Restic Secret")
+		return false, err
+	}
 
-	// define Restic secret to be created
-	newResticSecret := &corev1.Secret{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      fmt.Sprintf("%s-secret", vsb.Name),
-			Namespace: r.NamespacedName.Namespace,
-			Labels: map[string]string{
-				VSBLabel: vsb.Name,
-			},
-		},
-		Type: corev1.SecretTypeOpaque,
+	rsecret, err := resticcommon.PopulateResticSecret(&vsb, nil)
+	if err != nil {
+		return false, err
 	}
 
 	// Create Restic secret in OADP namespace
-	op, err := controllerutil.CreateOrUpdate(r.Context, r.Client, newResticSecret, func() error {
+	op, err := controllerutil.CreateOrUpdate(r.Context, r.Client, rsecret, func() error {
 
-		return r.buildResticSecret(newResticSecret, &vsb, &pvc)
+		return resticcommon.BuildResticSecret(&resticSecret, rsecret, &pvc)
 	})
 	if err != nil {
 		return false, err
 	}
 
 	// set created Restic repo to VSB status
-	vsb.Status.ResticRepository = string(newResticSecret.Data[ResticRepository])
+	vsb.Status.ResticRepository = string(rsecret.Data[ResticRepository])
 
 	// Update VSB status
 	err = r.Status().Update(context.Background(), &vsb)
@@ -95,64 +93,15 @@ func (r *VolumeSnapshotBackupReconciler) CreateResticSecret(log logr.Logger) (bo
 	}
 
 	if op == controllerutil.OperationResultCreated || op == controllerutil.OperationResultUpdated {
-		r.EventRecorder.Event(newResticSecret,
+		r.EventRecorder.Event(rsecret,
 			corev1.EventTypeNormal,
 			"ResticSecretBackupReconciled",
-			fmt.Sprintf("%s restic secret %s", op, newResticSecret.Name),
+			fmt.Sprintf("%s restic secret %s", op, rsecret.Name),
 		)
 	}
 	return true, nil
 }
 
-func (r *VolumeSnapshotBackupReconciler) buildResticSecret(secret *corev1.Secret, vsb *datamoverv1alpha1.VolumeSnapshotBackup, pvc *corev1.PersistentVolumeClaim) error {
-
-	// get restic secret from user
-	resticSecret := corev1.Secret{}
-	if err := r.Get(r.Context, types.NamespacedName{Namespace: r.NamespacedName.Namespace, Name: resticSecretName}, &resticSecret); err != nil {
-		r.Log.Error(err, "unable to fetch Restic Secret")
-		return err
-	}
-
-	// assign new restic secret values
-	for key, val := range resticSecret.Data {
-		switch {
-		case key == AWSAccessKey:
-			AWSAccessValue = val
-		case key == AWSSecretKey:
-			AWSSecretValue = val
-		case key == AWSDefaultRegion:
-			AWSDefaultRegionValue = val
-		case key == ResticPassword:
-			ResticPasswordValue = val
-		case key == ResticRepository:
-
-			// if trailing '/' in user-created Restic repo, remove it
-			stringVal := string(val)
-			stringVal = strings.TrimRight(stringVal, "/")
-
-			ResticRepoValue = stringVal
-		}
-	}
-
-	// create new repo path for snapshot
-	newRepoName := fmt.Sprintf("%s/%s/%s", ResticRepoValue, pvc.Namespace, pvc.Name)
-
-	// build new Restic secret
-	resticSecretData := &corev1.Secret{
-		Data: map[string][]byte{
-			AWSAccessKey:     AWSAccessValue,
-			AWSSecretKey:     AWSSecretValue,
-			AWSDefaultRegion: AWSDefaultRegionValue,
-			ResticPassword:   ResticPasswordValue,
-			ResticRepository: []byte(newRepoName),
-		},
-	}
-
-	secret.Data = resticSecretData.Data
-	return nil
-}
-
-// TODO: move these 2 functions to a common.go and check for VSB or VSR being used
 func (r *VolumeSnapshotRestoreReconciler) CreateVSRResticSecret(log logr.Logger) (bool, error) {
 	// get volumesnapshotrestore from cluster
 	vsr := datamoverv1alpha1.VolumeSnapshotRestore{}
@@ -160,23 +109,23 @@ func (r *VolumeSnapshotRestoreReconciler) CreateVSRResticSecret(log logr.Logger)
 		r.Log.Error(err, "unable to fetch VolumeSnapshotRestore CR")
 		return false, err
 	}
+	// get restic secret from user
+	resticSecret := corev1.Secret{}
+	if err := r.Get(r.Context, types.NamespacedName{Namespace: r.NamespacedName.Namespace, Name: resticSecretName}, &resticSecret); err != nil {
+		r.Log.Error(err, "unable to fetch Restic Secret")
+		return false, err
+	}
 
 	// define Restic secret to be created
-	newResticSecret := &corev1.Secret{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      fmt.Sprintf("%s-secret", vsr.Name),
-			Namespace: r.NamespacedName.Namespace,
-			Labels: map[string]string{
-				VSRLabel: vsr.Name,
-			},
-		},
-		Type: corev1.SecretTypeOpaque,
+	newResticSecret, err := resticcommon.PopulateResticSecret(nil, &vsr)
+	if err != nil {
+		return false, err
 	}
 
 	// Create Restic secret in OADP namespace
 	op, err := controllerutil.CreateOrUpdate(r.Context, r.Client, newResticSecret, func() error {
 
-		return r.buildVSRResticSecret(newResticSecret, &vsr)
+		return resticcommon.BuildVSRResticSecret(&resticSecret, newResticSecret, &vsr)
 	})
 	if err != nil {
 		return false, err
@@ -190,43 +139,4 @@ func (r *VolumeSnapshotRestoreReconciler) CreateVSRResticSecret(log logr.Logger)
 		)
 	}
 	return true, nil
-}
-
-// TODO: move these 2 functions to a common.go and check for VSB or VSR being used
-func (r *VolumeSnapshotRestoreReconciler) buildVSRResticSecret(secret *corev1.Secret, vsr *datamoverv1alpha1.VolumeSnapshotRestore) error {
-
-	// get restic secret from user
-	resticSecret := corev1.Secret{}
-	if err := r.Get(r.Context, types.NamespacedName{Namespace: r.NamespacedName.Namespace, Name: resticSecretName}, &resticSecret); err != nil {
-		r.Log.Error(err, "unable to fetch Restic Secret")
-		return err
-	}
-
-	// assign new restic secret values
-	for key, val := range resticSecret.Data {
-		switch {
-		case key == AWSAccessKey:
-			AWSAccessValue = val
-		case key == AWSSecretKey:
-			AWSSecretValue = val
-		case key == AWSDefaultRegion:
-			AWSDefaultRegionValue = val
-		case key == ResticPassword:
-			ResticPasswordValue = val
-		}
-	}
-
-	// build new Restic secret
-	resticSecretData := &corev1.Secret{
-		Data: map[string][]byte{
-			AWSAccessKey:     AWSAccessValue,
-			AWSSecretKey:     AWSSecretValue,
-			AWSDefaultRegion: AWSDefaultRegionValue,
-			ResticPassword:   ResticPasswordValue,
-			ResticRepository: []byte(vsr.Spec.VolumeSnapshotMoverBackupref.ResticRepository),
-		},
-	}
-
-	secret.Data = resticSecretData.Data
-	return nil
 }
