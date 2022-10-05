facc479f9fe5172fd5ae51c59b78047c7a05cde6
Emily McMullan <emcmulla@redhat.com>
2022-10-05 16:00:59 -0400

    Fail VSB/VSR after conditions error (#150)

--- controllers/volumesnapshotbackup_controller.go
+++ controllers/volumesnapshotbackup_controller.go
@@ -129,8 +129,8 @@ func (r *VolumeSnapshotBackupReconciler) Reconcile(ctx context.Context, req ctrl
 				Status:  metav1.ConditionFalse,
 				Reason:  ReconciledReasonError,
 				Message: err.Error(),
-			},
-		)
+			})
+		vsb.Status.Phase = volsnapmoverv1alpha1.SnapMoverBackupPhaseFailed
 
 	} else {
 		// Set complete status condition
